cb74186342d6347a45be77fb6924da805e1a998c
byrdeman00 <byrdeman@protonmail.com>
2023-06-05 06:50:41 -0400

    impliment argparser

--- encode-each.py
+++ encode-each.py
@@ -1,20 +1,38 @@
+#!/usr/bin/python
+import argparse
 import base64
 
 from os import path
 from sys import argv
 
-filename = argv[1] or 'mysql-blind'
+parser = argparse.ArgumentParser(prog="encode-each", description="Loop through lines of file and base64 encode each one")
+parser.add_argument('inputfile', type=argparse.FileType('r', encoding='UTF-8'))
+parser.add_argument('--outfile', type=argparse.FileType('w'), help='save results')
+parser.add_argument('--exceptions', type=argparse.FileType('w'), help='save exceptions')
 
-if path.isfile(filename):
-    file = open(filename)
-else:
-    raise Exception(f'File Not Found: {filename}')
+def encode(file, outfile, exceptionfile):
 
-lines = file.readlines()
+    lines = file.readlines()
+    results = ""
+    exceptions = ""
+    for line in lines:
+        try:
+            linebytes = line.encode('ascii')
+            b64line_bytes = base64.b64encode(linebytes)
+            b64line_string = b64line_bytes.decode('ascii')
+            if outfile:
+                results = results + f'{b64line_string}\n'
+            print(b64line_string)
+        except Exception as e:
+            if exceptionfile:
+                exceptions += line
+            print(f'could not encode {line}')
 
-for line in lines:
-    linebytes = line.encode('ascii')
-    b64line_bytes = base64.b64encode(linebytes)
-    b64line_string = b64line_bytes.decode('ascii')
-    print(b64line_string)
-
+    if outfile:
+        outfile.write(results)
+    if exceptionfile:
+        exceptionfile.write(exceptions)
+        
+if __name__ == '__main__':
+    args = parser.parse_args()
+    encode(args.inputfile, args.outfile, args.exceptions)
